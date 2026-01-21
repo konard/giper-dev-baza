36898c230efa0a37901548fb13b40c90e977d6b1
jin <jin@hyoo.ru>
2026-01-21 21:46:52 +0300

    --

--- app/app.web.ts
+++ app/app.web.ts
@@ -1,3 +1,3 @@
-// namespace $ {
-// 	$giper_baza_yard.masters = [ ... new Set([ $mol_dom_context.location.origin + '/', ... $giper_baza_yard.masters ]) ]
-// }
+namespace $ {
+	$giper_baza_yard.masters = [ ... new Set([ $mol_dom_context.location.origin + '/', ... $giper_baza_yard.masters ]) ]
+}
